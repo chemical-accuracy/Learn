5e6927a97a7e5f5848064623a74e713a63775d35
Chemical Accuracy <108434153+chemical-accuracy@users.noreply.github.com>
2022-07-21 22:44:52 -0700

    VSCode in install_ubuntu.sh

--- Linux/Install_ubuntu.sh
+++ Linux/Install_ubuntu.sh
@@ -78,6 +78,10 @@ conda install numpy sympy scipy matplotlib
 
 
 # 1. VS Code 
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+\rm -f packages.microsoft.gpg
 sudo apt install code
 #Extensions: (language support) Python, C/C++, Modern Fortran, 
 # (to connect via SSH) Remote - SSH
